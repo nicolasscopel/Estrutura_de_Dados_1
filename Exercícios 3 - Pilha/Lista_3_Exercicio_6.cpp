f9821ba3f3605867f2b5ad77baac7ad2db174e2c
nicolasscopel <nicolaspscopel@gmail.com>
2023-08-29 15:54:11 -0300

    lista 3 feita (-5)

--- Exercícios 3 - Pilha/Lista_3_Exercicio_6.cpp
+++ Exercícios 3 - Pilha/Lista_3_Exercicio_6.cpp
@@ -23,4 +23,84 @@ main()
 	srand(time(NULL));
 	setlocale (LC_ALL, "Portuguese");
 
+	Pilha p1;
+
+	int menu, valor, numero;
+	int tam = 32;
+
+	inicializaP(&p1,tam);
+
+
+	do
+	{
+	    system("cls");
+
+	    cout << "\n-------- MENU --------";
+	    cout << "\n0 - Sair             -";
+	    cout << "\n1 - Converter Número -";
+	    cout << "\n----------------------";
+	    cout << "\nSua Escolha: ";
+	    cin >> menu;
+
+	    switch(menu)
+	    {
+        case 0:
+            {
+                system("cls");
+
+                cout << "\nPrograma Finalizado" << endl;
+
+                system("pause");
+                break;
+            }
+
+        case 1:
+            {
+
+                system("cls");
+                inicializaP(&p1,tam);
+
+                cout << "\nInforme um numero para converter para binário: ";
+                cin >> numero;
+
+                cout << "\n0 - LSD" << endl;
+
+                while(numero != 0)
+                {
+                    valor = numero % 2;
+
+                    numero = numero / 2;
+
+                    empilhaP(&p1, valor);
+
+                }
+                mostraP(&p1);
+
+                destroiP(&p1);
+
+                system("pause");
+                break;
+
+            }
+        default:
+            {
+                system("cls");
+
+                cout << "\nOpção Inválida" << endl;
+
+                system("pause");
+                break;
+            }
+
+
+
+
+
+	    }
+
+
+
+
+	}while(menu != 0);
+
 }
--- Exercícios 3 - Pilha/Lista_3_Exercicio_7.cpp
+++ Exercícios 3 - Pilha/Lista_3_Exercicio_7.cpp
@@ -19,7 +19,68 @@ using namespace std;
 
 main()
 {
-	srand(time(NULL));
-	setlocale (LC_ALL, "Portuguese");
+    srand(time(NULL));
+    setlocale (LC_ALL, "Portuguese");
+
+    Pilha p1;
+    Pilha p2;
+
+    int tamp1 = 7, cont1;
+    int tamp2 = 5, cont2;
+    bool resultado;
+
+    inicializaP(&p1, tamp1);
+    empilhaP(&p1, 13);
+    empilhaP(&p1, 11);
+    empilhaP(&p1, 9);
+    empilhaP(&p1, 7);
+    empilhaP(&p1, 5);
+    empilhaP(&p1, 3);
+    empilhaP(&p1, 1);
+
+    inicializaP(&p2, tamp2);
+    empilhaP(&p2, 6);
+    empilhaP(&p2, 4);
+    empilhaP(&p2, 2);
+
+
+    mostraP(&p1);
+    mostraP(&p2);
+
+    cont1 = contP(&p1, tamp1);
+    cont2 = contP(&p2, tamp2);
+
+    int tamp3 = cont1 + cont2;
+
+    Pilha p3;
+    inicializaP(&p3, tamp3);
+
+    for(int i = 0; i < tamp3; i++)
+    {
+        int valor;
+
+        if(!vaziaP(&p1))
+        {
+            valor = desempilhaP(&p1);
+            empilhaP(&p3, valor);
+        }
+        else
+        {
+           continue;
+        }
+
+        if(!vaziaP(&p2))
+        {
+            valor = desempilhaP(&p2);
+            empilhaP(&p3, valor);
+        }
+        else
+        {
+            continue;
+        }
+
+    }
+
+    mostraP(&p3);
 
 }
