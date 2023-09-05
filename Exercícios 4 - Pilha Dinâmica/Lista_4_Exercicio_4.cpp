256811c33d5b4bfe7ceeeb2f66c5e39f420502a5
nicolasscopel <nicolaspscopel@gmail.com>
2023-09-05 15:54:00 -0300

    lista 4 pronta

--- Exercícios 4 - Pilha Dinâmica/Lista_4_Exercicio_4.cpp
+++ Exercícios 4 - Pilha Dinâmica/Lista_4_Exercicio_4.cpp
@@ -1,7 +1,7 @@
 /* 4. Empregando pilha, crie uma função para verificar se uma expressão composta apenas por chaves, colchetes e
 parênteses está ou não balanceada.
-a) Exemplo de expressões balanceadas: “[ { () () } {} ]”, “{ [ ( [ { } ] ) ] }”
-b) Exemplo de expressões não balanceadas: “{ [ ( } ] )”, “{ [ ) () ( ] }”
+a) Exemplo de expressões balanceadas: [ { () () } {} ], { [ ( [ { } ] ) ] }
+b) Exemplo de expressões não balanceadas: { [ ( } ] ), { [ ) () ( ] }
 */
 
 #include <iostream>
@@ -11,16 +11,140 @@ b) Exemplo de expressões não balanceadas: “{ [ ( } ] )”, “{ [ ) () ( ] }”
 #include <iomanip>
 #include <ctime>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
-#include "include/pilha.hpp"
+#include "include/pilha-dinamicachar.hpp"
+
+void verificaexpressao(string valor, int tam)
+{
+
+    char c = ' ';
+    char p = ' ';
+    bool balanceado = true;
+    Pilha p1;
+
+    for(int i = 0; i < tam; i++)
+    {
+        if(valor[i] == '(')
+        {
+            empilhaP(&p1, valor[i]);
+        }
+
+        if(valor[i] == '[')
+        {
+            empilhaP(&p1, valor[i]);
+        }
+
+        if(valor[i] == '{')
+        {
+            empilhaP(&p1, valor[i]);
+        }
+
+
+        if(valor[i] == ')')
+        {
+            if(vaziaP(&p1))
+            {
+                balanceado = false;
+                break;
+            }
+
+            p = espiaP(&p1);
+
+            if(p == '(')
+            {
+                desempilhaP(&p1);
+            }
+            else
+            {
+                balanceado = false;
+                break;
+
+            }
+        }
+
+        if(valor[i] == ']')
+        {
+            if(vaziaP(&p1))
+            {
+                balanceado = false;
+                break;
+            }
+
+            p = espiaP(&p1);
+
+            if(p == '[')
+            {
+                desempilhaP(&p1);
+            }
+            else
+            {
+                balanceado = false;
+                break;
+
+            }
+        }
+
+        if(valor[i] == '}')
+        {
+            if(vaziaP(&p1))
+            {
+                balanceado = false;
+                break;
+            }
+
+            p = espiaP(&p1);
+
+            if(p == '{')
+            {
+                desempilhaP(&p1);
+            }
+            else
+            {
+                balanceado = false;
+                break;
+
+            }
+        }
+
+    }
+
+    if(!vaziaP(&p1))
+        balanceado = false;
+
+
+mostraP(&p1);
+
+    if(balanceado == true)
+    {
+        cout << "\nA expressão esta balanceada! ";
+
+    }
+    else
+    {
+        cout << "\nA expressão não esta balanceada! ";
+    }
+
+}
 
 
 
 main()
 {
-	srand(time(NULL));
-	setlocale (LC_ALL, "Portuguese");
+    srand(time(NULL));
+    setlocale (LC_ALL, "Portuguese");
+
+    string valor;
+    int tam;
+
+
+
+    cout << "\nInforme uma expressão para verificar: ";
+    getline(cin,valor);
+    tam = valor.size();
+
+    verificaexpressao(valor,tam);
 
 }
--- Exercícios 4 - Pilha Dinâmica/Lista_4_Exercicio_5.cpp
+++ Exercícios 4 - Pilha Dinâmica/Lista_4_Exercicio_5.cpp
@@ -12,7 +12,29 @@ ou seja, após a execução da função, P1 e P2 devem possuir os mesmos elementos e 
 
 using namespace std;
 
-#include "include/pilha.hpp"
+#include "include/pilha-dinamica.hpp"
+
+void copiaP(Pilha *p1, Pilha *p2)
+{
+    Pilha aux;
+    int valor;
+
+    while(!vaziaP(p1))
+    {
+        valor = desempilhaP(p1);
+        empilhaP(&aux, valor);
+    }
+
+    while(!vaziaP(&aux))
+    {
+        valor = desempilhaP(&aux);
+        empilhaP(p1, valor);
+        empilhaP(p2, valor);
+    }
+
+    destroiP(&aux);
+
+}
 
 
 
@@ -20,5 +42,90 @@ main()
 {
 	srand(time(NULL));
 	setlocale (LC_ALL, "Portuguese");
+
+
+    Pilha p1;
+    Pilha p2;
+    int valor, menu;
+
+    do
+    {
+
+        system("cls");
+        cout << "\n-------- MENU --------";
+        cout << "\n0 - Sair             -";
+        cout << "\n1 - Gerar Pilha P1   -";
+        cout << "\n2 - Copiar Pilha     -";
+        cout << "\n----------------------";
+        cout << "\nSua Escolha: ";
+        cin >> menu;
+
+        switch(menu)
+        {
+        case 0:
+        {
+            system("cls");
+            cout << "\nFinalizando Programa!" << endl;
+
+            destroiP(&p1);
+            destroiP(&p2);
+
+            system("pause");
+            break;
+        }
+
+        case 1:
+        {
+            destroiP(&p1);
+            destroiP(&p2);
+
+            system("cls");
+            cout << "\nInforme quantos valores deseja que sejam empilhados automaticamente: ";
+            cin >> valor;
+
+            for(int i = 0; i < valor; i++)
+            {
+                empilhaP(&p1, rand() % 50);
+
+            }
+            mostraP(&p1);
+
+            system("pause");
+            break;
+
+        }
+        case 2:
+        {
+            destroiP(&p2);
+            system("cls");
+            cout << "\nCopiando valores de P1 para P2" << endl;
+
+            copiaP(&p1, &p2);
+            cout << "\nP1 ";
+            mostraP(&p1);
+            cout << "\nP2 ";
+            mostraP(&p2);
+
+
+            system("pause");
+            break;
+
+        }
+
+        default:
+        {
+            system("cls");
+            cout << "\nOpção Incorreta!";
+
+            system("pause");
+            break;
+
+        }
+        }
+
+    }
+    while(menu != 0 );
+
+
 
 }
