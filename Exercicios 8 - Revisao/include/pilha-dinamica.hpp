a23387f6479781541d5ff9657a05b41f918581ee
nicolasscopel <nicolaspscopel@gmail.com>
2023-09-27 18:50:07 -0300

    Lista 8 - Revisão

---
+++ Exercicios 8 - Revisao/include/pilha-dinamica.hpp
@@ -0,0 +1,129 @@
+#ifndef _HPP_PILHA_DINAMICA
+#define _HPP_PILHA_DINAMICA
+
+#include <iomanip> //biblioteca usada para formatar a impressão da pilha
+
+
+struct NoPilha
+{
+    int dado; //informação do nó
+    NoPilha *prox; //próximo elemento
+};
+
+struct Pilha
+{
+    NoPilha *topo;
+
+    Pilha(){ //Construtor. Inicialização da pilha
+        topo = nullptr;
+    }
+};
+
+
+bool vaziaP(Pilha *p)
+{
+    if (p->topo == NULL)
+        return true;
+    else
+        return false;
+}
+
+
+//push
+bool empilhaP(Pilha *p, int dado)
+{
+    NoPilha *novo =  new NoPilha(); //cria um novo nó
+    if (novo == NULL) /// sistema não conseguiu alocar a memória
+        return false;
+
+    novo->dado = dado; //armazena a informação no nó
+    novo->prox = p->topo; //o próximo elemento do nó criado será o último elemento da pilha
+    p->topo = novo; //atualiza o topo da pilha para o nó criado.
+    return true;
+}
+
+//pop
+int desempilhaP(Pilha *p)
+{
+    int dado = 0;
+
+    // se não estiver vazia, retira valor
+    if (vaziaP(p) == false)
+    {
+        dado = p->topo->dado; //pega o dado armazenado no nó do topo
+        NoPilha *apagar = p->topo; //guarda o nó do topo em uma variável auxiliar;
+        p->topo = p->topo->prox; //atualiza o topo para o próximo elemento;
+
+        delete apagar;  /// libera a memória
+    }
+
+    return dado;
+}
+
+//peek
+int espiaP(Pilha* p)
+{
+    int dado = 0;
+
+    if (vaziaP(p) == false)
+        dado = p->topo->dado;
+
+    return dado;
+}
+
+
+void mostraP(Pilha *p)
+{
+    cout << "PILHA: " << endl;
+
+    if(vaziaP(p) == false)
+    {
+        cout << "--------------------------------------------" << endl;
+        cout << setfill(' ') << std::setw(13) << "Nó" << " | ";
+        cout << setfill(' ') << std::setw(13) << "Prox" << " | ";
+        cout << setfill(' ') << std::setw(10) << "Dado" << " |" << endl;
+        cout << "--------------------------------------------" << endl;
+        NoPilha *no = p->topo;
+        while (no != NULL)
+        {
+            cout << setfill(' ') << std::setw(13) << no << " | ";
+            cout << setfill(' ') << std::setw(13) << no->prox << " | ";
+            cout << setfill(' ') << std::setw(10) << no->dado << " |" << endl;
+
+            no = no->prox;
+        }
+        cout << "--------------------------------------------" << endl;
+    }
+}
+
+/// retorna true se o valor existe na pilha
+/// retorna false se o valor não existe na pilha
+bool buscaP(Pilha *p, int dado)
+{
+
+    NoPilha *no = p->topo;
+    while (no != NULL)
+    {
+        if(no->dado == dado)
+            return true;
+
+        no = no->prox;
+    }
+
+    return false;
+
+}
+
+void destroiP(Pilha *p)
+{
+    int dado;
+    while(vaziaP(p) == false)
+        dado = desempilhaP(p); //desempilha e descarta o valor desempilhado
+}
+
+#endif // _HPP_PILHA_DINAMICA
+
+
+
+
+
